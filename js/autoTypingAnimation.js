f8f2451bc3534588e14096caaa19da085a870fb5
syahrulramadan <syahrulramadan3936@gmail.com>
2024-09-05 20:31:25 +0800

    first commit

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Syahrul Ramadan</span> Web Development adalah seni dan ilmu membangun dan mengelola situs web yang efektif dan responsif. Sebagai web developer, saya menggabungkan kreativitas desain dengan logika pemrograman untuk menciptakan pengalaman pengguna yang intuitif dan menarik. <p/>Dengan menggunakan berbagai bahasa pemrograman seperti HTML, CSS, dan JavaScript, saya memastikan setiap situs yang saya bangun tidak hanya terlihat bagus tetapi juga fungsional dan mudah diakses di berbagai perangkat.</p>Dalam setiap proyek, saya fokus pada kebutuhan pengguna dan tujuan bisnis. Saya berpengalaman dalam menggunakan berbagai framework dan alat seperti React, Bootstrap, dan Node.js untuk membangun aplikasi web yang dinamis dan interaktif. Selain itu, saya juga memiliki pemahaman yang kuat tentang prinsip UX/UI, yang memungkinkan saya untuk menciptakan antarmuka yang memudahkan pengguna dalam menjelajahi situs web dengan lancar dan efisien <p/>Dengan latar belakang yang kuat dalam Web Development, saya terus berupaya untuk belajar dan berkembang, mengikuti tren teknologi terbaru untuk memastikan bahwa situs web yang saya buat selalu mutakhir dan relevan. Saya percaya bahwa situs web adalah wajah digital dari sebuah bisnis, dan dengan keahlian yang saya miliki, saya siap membantu Anda menghadirkan citra yang profesional dan menarik di dunia online.</span>"
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
